03309690a7aded167605835a2a055313ef879c47
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2019-08-25 04:37:10 +0900

    Divide performGet function

--- cmd/in/main.go
+++ cmd/in/main.go
@@ -15,6 +15,36 @@ func writeFileString(filePath, content string) error {
 	return ioutil.WriteFile(filePath, []byte(content), 0644)
 }
 
+func performGet(request models.GetRequest, basePath string, redisConn redis.Conn) (*models.GetResponse, error) {
+	ret, err := redisConn.Do("XRANGE", request.Source.Key, request.Version.ID, request.Version.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	record := ret.([]interface{})[0].([]interface{})
+	id := string(record[0].([]byte))
+
+	fields, err := redis.StringMap(record[1], nil)
+	if err != nil {
+		return nil, err
+	}
+
+	metadata := make([]models.MetadataEntry, 0)
+	for _, fieldName := range request.Source.Fields {
+		value := fields[fieldName]
+		if err := writeFileString(filepath.Join(basePath, fieldName), value); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s\n", err)
+			return nil, err
+		}
+		metadata = append(metadata, models.MetadataEntry{Name: fieldName, Value: value})
+	}
+
+	return &models.GetResponse{
+		Version:  models.Version{ID: id},
+		Metadata: metadata,
+	}, nil
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "error: missing argument")
@@ -34,33 +64,10 @@ func main() {
 		panic(err)
 	}
 
-	ret, err := conn.Do("XRANGE", request.Source.Key, request.Version.ID, request.Version.ID)
+	response, err := performGet(request, basePath, conn)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
 		panic(err)
-	}
-
-	record := ret.([]interface{})[0].([]interface{})
-	id := string(record[0].([]byte))
-
-	fields, err := redis.StringMap(record[1], nil)
-	if err != nil {
-		panic(err)
-	}
-
-	metadata := make([]models.MetadataEntry, 0)
-	for _, fieldName := range request.Source.Fields {
-		value := fields[fieldName]
-		if err := writeFileString(filepath.Join(basePath, fieldName), value); err != nil {
-			fmt.Fprintf(os.Stderr, "error: %s\n", err)
-			panic(err)
-		}
-		metadata = append(metadata, models.MetadataEntry{Name: fieldName, Value: value})
-	}
-
-	response := models.GetResponse{
-		Version:  models.Version{ID: id},
-		Metadata: metadata,
 	}
 
 	if err := json.NewEncoder(os.Stdout).Encode(&response); err != nil {
